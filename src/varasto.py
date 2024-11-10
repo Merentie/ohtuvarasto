8984081cc3b7420b23feb0f13a7da5f06509de06
Merentie <mikael.merentie@gmail.com>
2024-11-10 16:37:58 +0200

    Fix error

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. abababababababababababababababababaasafdsafdsafdsafdsfdsfdsfds
+    # huom: ominaisuus voidaan myös laskea.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
